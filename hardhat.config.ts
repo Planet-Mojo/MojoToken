41a6929e3798b07c103d93bedb6b9b36f84b8bc0
nerfZael <bogunovij@gmail.com>
2024-02-07 13:32:40 +0100

    added required layer zero deps for testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.24",
+        version: "0.8.22",
         settings: {
           optimizer: {
             enabled: true,
